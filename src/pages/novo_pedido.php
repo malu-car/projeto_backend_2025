284f00476ba903040d7a8731de5ea062577ccb57
Luis Felipe <luisfelipevzp100@gmail.com>
2026-01-20 18:41:51 -0300

    correção do formulário automático (link)

--- src/pages/novo_pedido.php
+++ src/pages/novo_pedido.php
@@ -99,7 +99,7 @@
         				  {
           					echo "<option value='comissario' selected>Comissário</option>";
           					echo "<script>const selectCanalVenda = document.getElementById('canal_venda');
-            						  selectCanalVenda.setAttribute('disabled','true');
+            						  selectCanalVenda.setAttribute('onchange','this.value = ". $linha['id'] ."');
             						  selectCanalVenda.setAttribute('style','background-color: var(--grey-50); color: black');</script>";
         				  }
         				  else {
@@ -125,7 +125,7 @@
           					{
           						echo "<option value='". $linha['id'] ."' selected>". $linha['nome'] ."</option>";
           						echo "<script>const selectSetor = document.getElementById('setor');
-          						      selectSetor.setAttribute('disabled','true');
+          						      selectSetor.setAttribute('onchange','this.value = ". $linha['id'] ."');
           							    selectSetor.setAttribute('style','background-color: var(--grey-50); color: black');</script>";
           					}
         				  }
@@ -161,7 +161,7 @@
             					{
             						echo "<option value='". $linha['id'] ."' selected>". $linha['setor_nome']." - R$ ".$linha['preco'] ."</option>";
             						echo "<script>const selectLote = document.getElementById('lote');
-            						      selectLote.setAttribute('disabled','true');
+            						      selectLote.setAttribute('onchange','this.value = ". $linha['id'] ."');
             							    selectLote.setAttribute('style','background-color: var(--grey-50); color: black');</script>";
             					}
           				  }
